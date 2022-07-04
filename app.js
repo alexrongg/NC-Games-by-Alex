70d15047ca87bc8024d74c724062cbe567305c20
alexrongg <alexrong19@gmail.com>
2022-07-04 14:35:42 +0100

    Added 404 endpoint and server errror handling

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ const app = express();
 
 app.get("/api/categories", getCategories);
 
+app.use("*", (req, res) => {
+    res.status(404).send({msg: "Invalid path"});
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send({msg: "Internal server error"});
+});
+
 module.exports = app;
