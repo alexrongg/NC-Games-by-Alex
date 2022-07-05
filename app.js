bb1a2aedb7b16d5e3a21c1d08168eb114a800374
alexrongg <alexrong19@gmail.com>
2022-07-05 14:23:46 +0100

    Altered error check to be more generic

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use((err, req, res, next) => {
         res.status(404).send(err)
     } else if (err.code === "22P02") {
         res.status(400).send({ msg: "Invalid Syntax of review ID, need to be a number"})
-    } else if (err.msg === "Patched object must have the form of { inc_votes: newVote } where newVote indicates the change in votes. Votes cannot go below 0") {
+    } else if (err.msg) {
         res.status(400).send(err)
     } else {
         next(err);
