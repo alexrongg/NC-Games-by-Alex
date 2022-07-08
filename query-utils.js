81d690c0ce5870fe38389ab26e93499e83159d85
alexrongg <alexrong19@gmail.com>
2022-07-08 11:45:44 +0100

    Added tests for checkCategoryExists

--- query-utils.js
+++ query-utils.js
@@ -2,7 +2,6 @@ const connection = require("./db/connection");
 
 exports.checkCategoryExists = (category) => {
 	return connection.query(`SELECT * FROM categories WHERE slug = $1;`, [category]).then((results) => {
-        console.log(results.rows)
 		if (results.rows.length !== 0) {
 			return true;
 		};
